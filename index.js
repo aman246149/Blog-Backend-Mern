de7a3267158db470803d19ff77eed42fe87e2781
Aman <amanthapliyal14@gmail.com>
2021-08-04 23:23:08 +0530

    fixed bugs

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const cors = require('cors');
-const PORT=process.env.PORT || 5000
+const PORT=process.env.PORT  || 5000
 
 //import routers
 const authRoute = require("./routes/auth");
